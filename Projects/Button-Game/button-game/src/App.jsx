623df3a08b7880787ceb214e933edf2c4918f9a6
ayanaggarwal006 <ayanaggarw4@gmail.com>
2024-05-01 12:26:07 +0530

    Usestate hooks

---
+++ Projects/Button-Game/button-game/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'   //hooks in react
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+  // let counter = 15
+
+  const addValue = () => {
+    setCounter(counter + 1)
+  }
+  const reduceValue = () => {
+    if(counter-1 >=0) 
+    setCounter(counter - 1)
+  }
+  return (
+    <>
+    <h1>Chai aur code</h1>
+    <h2>Counter Value : {counter}</h2>
+    <button onClick={addValue}>Add Value</button>
+    <button onClick={reduceValue}>Reduce Value</button>
+     </>
+  )
+}
+
+export default App
